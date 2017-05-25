2e1e70d5987a7784f62ef53049bd8e33e8797d17
steven <stevenle2011@gmail.com>
2017-05-25 10:43:42 +0700

    add redux-logger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from 'react';  
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';  
-import {App} from './indexApp.js';  
+ 
 
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>Hello World!</p>
+      </div>
+    );
+  }
+
+}
 
 ReactDOM.render(  
 	 <App />,
